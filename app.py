4402baf8720f38c4424bf76ee1b50cf0833a73d0
Parkjasper <127505672+Parkjasper@users.noreply.github.com>
2024-05-07 12:38:47 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import joblib
 
 # Load the trained model
-model = joblib.load('Frauddetection.pkl')
+model = joblib.load('frauddetection.pkl')
 
 # Define the Streamlit app
 st.title("Financial Transaction Fraud Detection")
@@ -17,36 +17,38 @@ Welcome to our Credit Card Fraud Detection Web App! This app aims to detect frau
 **Contributors:** Jerin Jasper, Selvalakshmi, Santhiya, Aganiya
 """)
 
-# Input features of the transaction
-st.sidebar.header('Transaction Details')
-amount = st.sidebar.number_input("Transaction Amount (Rs)", min_value=0.0, max_value=110000.0)
-oldbalanceOrg = st.sidebar.number_input("Old Balance Orig (Rs)", min_value=0.0, max_value=110000.0)
-oldbalanceDest = st.sidebar.number_input("Old Balance Dest (Rs)", min_value=0.0, max_value=110000.0)
-newbalanceOrig = st.sidebar.number_input("New Balance Orig (Rs)", min_value=0.0, max_value=110000.0)
-newbalanceDest = st.sidebar.number_input("New Balance Dest (Rs)", min_value=0.0, max_value=110000.0)
-transaction_type = st.sidebar.selectbox("Transaction Type", 
-                                       {"CASH_IN": 0, "CASH_OUT": 1, "DEBIT": 2, "PAYMENT": 3, "TRANSFER": 4})
+# Upload CSV file containing transaction details
+st.sidebar.header('Upload Transaction Details')
+file = st.sidebar.file_uploader("Upload CSV", type=["csv"])
 
 # Prediction function
-def predict(amount, oldbalanceOrg, newbalanceOrig, oldbalanceDest, newbalanceDest, transaction_type):
+def predict(transaction_data):
     # Make prediction
-    features = np.array([[amount, oldbalanceOrg, newbalanceOrig, oldbalanceDest, newbalanceDest, transaction_type]])
-    prediction = model.predict(features)
+    prediction = model.predict(transaction_data)
     return "Fraudulent" if prediction == 1 else "Not Fraudulent"
 
 # Detection result
-if st.button("Detect Fraud"):
-    result = predict(amount, oldbalanceOrg, newbalanceOrig, oldbalanceDest, newbalanceDest, transaction_type)
+if file is not None:
+    df = pd.read_csv(file)
+    st.write("Uploaded Transaction Details:")
+    st.write(df)
+
+    # Select relevant features
+    features = df[['step', 'amount', 'oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest',
+                   'newbalanceDest', 'isFlaggedFraud', 'Type']]
+
+    # Make prediction
+    result = predict(features.values)
     st.write(f"**Prediction:** The transaction is **{result}**.")
 
 # Display some sample data
 st.write("""
 ## Sample Transaction Details
 Here are some sample transaction details that you can use for testing:
-- Amount: 3478.18 Rs
-- Old Balance Orig: 19853.00 Rs
-- New Balance Orig: 16374.82 Rs
-- Old Balance Dest: 0.00 Rs
-- New Balance Dest: 0.00 Rs
+- Amount: 3478.18
+- Old Balance Orig: 19853.00
+- New Balance Orig: 16374.82
+- Old Balance Dest: 0.00
+- New Balance Dest: 0.00
 - Transaction Type: CASH_OUT
 """)
